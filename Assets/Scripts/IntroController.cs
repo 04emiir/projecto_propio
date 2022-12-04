7ff8d1dc9dc5bb71655ada3a5d6b39b1e2e77990
emiliowebdev <emilio.nero3@gmail.com>
2022-12-05 00:40:50 +0100

    a

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -17,7 +17,6 @@ public class IntroController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         if (introText.color.a <= 0.9f)
             introText.color = new Color(1f, 1f, 1f, introText.color.a + (0.5f * Time.deltaTime));
         else
@@ -25,18 +24,7 @@ public class IntroController : MonoBehaviour
     }
 
     IEnumerator LoadGame() { 
-        yield return new WaitForSeconds(3f);
-=======
-        if (introText.color.a != 1f)
-            introText.color = new Color(1f, 1f, 1f, introText.color.a + (0.5f * Time.deltaTime));
-        else
-            soundEffect.Play();
-            StartCoroutine("loadGame");
-    }
-
-    IEnumerator loadGame() { 
         yield return new WaitForSeconds(2f);
->>>>>>> f00ade70ef5ba4871d14d57e8e8bc49b76750823
         SceneManager.LoadScene("MenuScene");
     }
 
